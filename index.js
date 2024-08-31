9ee1dd87671ef4b612ad9ccfc003c022ec1952a6
pankaj <pankaj.dosso21@gmail.com>
2024-08-31 18:00:55 +0530

    asd

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use('/',(req,res)=>{
+  console.log("i said hello");
+})
 app.use('/uploads', express.static('uploads'));
 app.use((req, res) => {
   res.status(404).json({ message: 'You are hitting the wrong API URL' });
